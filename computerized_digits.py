c3a2a088f1f13a233b05e8111b900b932ef449dd
Carlos Dip <cadudip@hotmail.com>
2021-11-23 18:42:49 -0300

    Script to create additional data.

---
+++ computerized_digits.py
@@ -0,0 +1,40 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+from typing import Tuple, List
+import numpy as np
+import matplotlib.pyplot as plt
+fonts = ['Cambria.ttc', 'Calibril.ttf', 'Consola.ttf', 'Cour.ttf', 'Arial.ttf', 'Segoeuil.ttf']
+digits = [str(x) for x in range(10)]
+
+def make_image(center: Tuple[int, int], text: str, font: str, font_size: int):
+    img = Image.new('L', (28,28))
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("arial.ttf", 20)
+    draw.text(center,text,(255),font=font, anchor='mm', align='center')
+    # img.show()
+    return img
+
+def make_training_array(amnt: int):
+    labels = {str(x):[] for x in range(10)}
+    for i in range(amnt):
+        for label in digits:
+            font = np.random.choice(fonts)
+            font_size = np.random.randint(18,22)
+            x,y = np.random.randint(-4,4,2)
+            img = make_image((14+x,14+y), label, font, font_size).rotate(np.random.randint(-15,15))
+            labels[label].append(img.copy())
+    return labels
+
+def plot_examples(arr: List):
+    plt.figure(figsize=(20,10))
+    columns = 5
+    for i, image in enumerate(arr):
+        plt.subplot(len(arr) // columns + 1, columns, i + 1)
+        plt.imshow(image, cmap='gray')
+    plt.show()
+
+
+if __name__ == '__main__':
+    train = make_training_array(50)
+    plot_examples(train['4'][:10])
